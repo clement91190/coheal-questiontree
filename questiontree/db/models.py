e78f33790809c6760be05bcd8a97fa758a981e94
clement91190 <clem.jambou@gmail.com>
2013-10-10 17:35:58 +0100

    stupid script to fill database based on the list of symptome

--- questiontree/db/models.py
+++ questiontree/db/models.py
@@ -1,8 +1,6 @@
 # -*-coding:Utf-8 -*
-
 from mongoengine import Document, StringField, IntField, ListField,\
     register_connection
-import urllib2
 
 
 QUESTION_URI = 'mongodb://clement91190:rafiki@lafleur.mongohq.com:10078/app18535327'
@@ -13,18 +11,15 @@ print "connecting to question-tree database"
 register_connection(QUESTION_ALIAS, 'question-tree-production', host=QUESTION_URI)
 
 
-
 class Question(Document):
     """
     Generic Document for questions.
     """
     meta = {
-            'db_alias':'question-tree-production',
-            'collection': 'question'}
+        'db_alias': 'question-tree-production',
+        'collection': 'question'}
     q_id = IntField()
     q_type = IntField()
     tags = ListField()
     symptome = StringField()
     question_text = StringField()
-
-
