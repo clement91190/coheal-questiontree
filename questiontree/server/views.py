0a5a163cbc428fc60a1a8198853a7c98202712b9
clement91190 <clem.jambou@gmail.com>
2013-10-14 18:21:39 +0100

    ajax query done for one symptome

--- questiontree/server/views.py
+++ questiontree/server/views.py
@@ -1,7 +1,7 @@
-from flask import render_template
+from flask import render_template, request
 from questiontree.server import app 
 from questiontree.db import models
-
+import traceback
 
 @app.route('/modify')
 def modify():
@@ -10,6 +10,18 @@ def modify():
     return render_template(
         'modify.html',
         questions=questions)
+
+
+@app.route('/modifyone')
+def modifyone():
+    try:
+        id = request.args.get('id')
+        id = int(id)
+        question = models.Question.objects(q_id=id).first()
+        return render_template('modify_one.html', question=question)
+    except:
+        traceback.print_exc()
+        return "not good" 
 
 
 @app.route('/help')
@@ -22,11 +34,18 @@ def simulate():
     return render_template('simulate.html')
 
 
-@app.route('/findandmodify')
+@app.route('/findandmodify', methods=['POST'])
 def findandmodify():
-    question = models.Question.objects(q_id=models.Question.TYPE_SYMPTOME).first()
-    return question.to_json()
-
+    try: 
+        q_id =  request.form['q_id']
+        qtext =  request.form['qtext']
+        question = models.Question.objects(q_id=q_id).first()
+        question.question_text = qtext
+        question.save()
+        return question.to_json()
+    except:
+        print traceback.print_exc()
+        return False
 
 @app.route('/test')
 def test():
