9bb6da68dc54f150a576cde41a60ab31c9ad0184
clement91190 <clem.jambou@gmail.com>
2013-10-28 14:21:55 +0000

    first batch of the Graph and tree structures

--- questiontree/db/models.py
+++ questiontree/db/models.py
@@ -56,3 +56,29 @@ class Tag(Document):
             i.delete()
 
 
+class Graph(Document):
+    """ Graph is a collection of Question with a Node/edges structure """
+    q_id = IntField() 
+    edges = ListField()
+    weight = ListField()
+
+    def get_question(self):
+        """ return the question corresponding to q_id"""
+        return Question.objects(q_id=self.q_id).first()
+
+    def follow_edge(self, ind=1):
+        """ follow the edge and return the node """
+        node = Graph.objects(q_id=self.edges[ind])
+        return node
+
+
+class TreeStructure(Document):
+    q_id = IntField()
+    children = ListField()
+    # float numbers to determine logical movement inside the tree
+    treshold = ListField() 
+    
+    @staticmethod
+    def get_tree():
+        """return the tree using JSON format"""
+        pass
